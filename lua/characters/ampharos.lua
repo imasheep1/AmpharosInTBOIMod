b6a97aa5458313e2ba32465526d02c8375f90b2c
IAGOnDiscord <122576313+IAGOnDiscord@users.noreply.github.com>
2023-03-13 08:40:34 -0400

    Remove unnecessary logging

--- lua/characters/ampharos.lua
+++ lua/characters/ampharos.lua
@@ -1,6 +1,5 @@
 -- Pure Ampharos character data. Conf at top, code at bottom.
 -- Derived from wofsauge's character template. I have no shame.
-print("Ampharos loaded")
 
 -- Stats are NOT multipliers over Isaac's default stats. They are directly set.
 local stats = {
