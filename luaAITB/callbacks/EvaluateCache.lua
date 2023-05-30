9d67dcefa28fc724e0c73ec1f8f118305d9e54b3
IAGOnGithub <122576313+IAGOnGithub@users.noreply.github.com>
2023-05-30 16:52:10 -0400

    delete now depreciated luaAITB directory
    It has been supplanted by luaAmpharosMod. I should run git rm more often...

--- luaAITB/callbacks/EvaluateCache.lua
+++
@@ -1,6 +0,0 @@
-local sheepStats = require("luaAITB.characters.SheepStats")
-
-modAITB:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, function(_, player, cacheFlag)
-    sheepStats:CalcStats(player, cacheFlag)
-end)
-
--- luaAITB/callbacks/PostPlayerInit.lua
+++
@@ -1,6 +0,0 @@
-local sheepCosts = require("luaAITB.characters.SheepCosts")
-
-modAITB:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, function(_, player)
-    sheepCosts:ApplyCosts(player)
-end)
-
